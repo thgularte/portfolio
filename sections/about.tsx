f628dd0e9516ad79d6a7fc7bbfe57cbf67575dd0
Theodor <47986817+thgularte@users.noreply.github.com>
2026-01-27 16:29:02 -0300

    fix: separando conteudo do componente
    Separei variavel do conteudo textual da pagina nas pasta content só importando onde foi usado

--- sections/about.tsx
+++ sections/about.tsx
@@ -2,26 +2,10 @@ import Image from "next/image";
 import SectionHeader from "@/components/Section_Header";
 import HobbiesSection from "@/components/ui/dropHobbies";
 import { Container } from "@/components/layout/container";
+import { profileData } from "@/content/about";
 
 export default function SectionAbout() {
-  const profileData = [
-    {
-      title: "Hobbies",
-      content:
-        "Meus hobbies são assistir os jogos do meu time Sport Club Internacional, assistir filmes, buscando sempre filmes que sejam interessantes e marcantes, ou só de terror mesmo.",
-    },
-    {
-      title: "Diversão",
-      content:
-        "Para me divertir costumo ir ao cinema, passear com meu fusca curtindo uma música usando meus CD's. Assim como curto assistir vários filmes aleatórios de atores e atrizes que gosto, jogar alguns jogos de video game mais antigo.",
-    },
-    {
-      title: "Descrição física",
-      content:
-        "Sou um homem jovem, de pele clara, cabelos curtos e loiros, levemente cacheados, e olhos azuis. Costumo usar roupas casuais, como camisetas de cores lisas e poucos detalhes. Tenho uma expressão tranquila, com um leve sorriso. Minha estatura é mediana e meu rosto é arredondado.",
-    },
-  ];
-
+  
   return (
     <section className="mt-20">
       <Container>
